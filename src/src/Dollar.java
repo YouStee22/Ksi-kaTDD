5a90f371aa36a03ec713b5164f38f877242b2cf1
YouStee22 <tomekpiorko2000@wp.pl>
2023-02-20 12:20:23 +0100

    Rozdział 12

--- src/src/Dollar.java
+++ src/src/Dollar.java
@@ -1,53 +1,57 @@
-class Dollar extends Money{
-
-    private String currency;                                    //dodane od 9
-    public Dollar(int amount, String currency) {
-       super(amount, currency);                                      //dodane 9
-    }
-    public Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
-    }
-}
-
-
-
-class Franc extends Money{
-    private String currency;                                    //dodane od 9
-
-    public Franc(int amount, String currency) {
-        super(amount, currency);                                      //dodane od 9
-    }
-    public Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
-    }
-}
+//class Dollar extends Money{
+//
+//    private String currency;
+//    public Dollar(int amount, String currency) {
+//       super(amount, currency);
+//    }
+//    static Money dollar (int amount){
+//        return new Money(amount, "USD");
+//    }
+//}
 
 
 
+//class Franc extends Money{
+//
+//    private String currency;
+//    public Franc(int amount, String currency) {
+//        super(amount, currency);
+//    }
+//    static Money franc (int amount){
+//        return new Franc(amount, "USD");
+//    }
+//}
 
-abstract class Money {
+
+
+
+ class Money {
 
     public Money (int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
     protected int amount;
-    protected String currency;                                   //dodane od 9 rozdzilu
-    abstract Money times(int multipliter);                          //to wgl potrzebne?
-
+    protected String currency;
     String currency() {
         return currency;
     }
+    public String toString() {
+        return amount + " " + currency;
+    }
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
 
+    }
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
-    }
+        return new Money(amount, "CHF");
 
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 }
--- src/test/Test1.java
+++ src/test/Test1.java
@@ -11,19 +11,9 @@ public class Test1{
         assertEquals(Money.dollar(15), five.times(3));
     }
     @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -32,5 +22,4 @@ public class Test1{
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
-
 }
